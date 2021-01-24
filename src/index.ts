64aa98958cd61e9fb284070ab4b01d002fa743e4
Ryan Delaney <vitineth@gmail.com>
2021-01-24 16:55:25 +0000

    Fixing file locations and adding clean commands

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ let messager: RabbitNetworkHandler<any, any, any, any, any, any> | undefined;
 let database: EquipmentDatabase | undefined;
 let configuration: z.infer<typeof ConfigurationSchema> | undefined;
 
-fs.readFile(path.join(__dirname, '..', 'config', 'configuration.json'), { encoding: 'utf8' })
+fs.readFile(path.join(__dirname, '..', '..', 'config', 'configuration.json'), { encoding: 'utf8' })
     .then((file) => {
         __.debug('loaded configuration file');
 
